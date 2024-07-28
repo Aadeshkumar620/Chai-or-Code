57fcf474c83862ebcf097918d8e53b61dcaf635b
Aadesh kumar <162555223+Aadeshkumar620@users.noreply.github.com>
2024-07-28 19:48:29 +0000

    commit

---
+++ 1_basics/variable.js
@@ -0,0 +1,12 @@
+const accountId = 123456;
+let accountEmail = "ak6202292434@gmail.com";
+var accountPassword = 147852369;
+let accountCity = "Patna";
+let city;
+
+accountCity = "Mumbai"
+
+console.log(accountEmail);
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,city]);
+
--- 1_variable.js
+++
@@ -1,6 +0,0 @@
-const accountId = 12365;
-let accountEmail = "ak6202292434@gmail.com";
-var accountPassword = "123456";
-accountCity = "Jaipur";
-//accountId = 2; note allowed
-console.log("Aadesh kumar");
